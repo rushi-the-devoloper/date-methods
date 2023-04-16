8fc58903c4d707c13aca0a21ee48f878d99474e0
rushi-reactdev <rushikeshkale669@gmail.com>
2023-04-16 23:48:46 +0530

    here is the problems that i solve by learning javascript date methods

---
+++ script.js
@@ -0,0 +1,20 @@
+let x = new Date("april,10,2023 10:30:20");
+let y = new Date();
+console.log(x);
+console.log(y);
+if(x>y){
+    console.log("y is the past date")
+}
+else if(x<y){
+    console.log("y is the future date")
+}
+else{
+    console.log("x and why are the same dates")
+}
+
+
+
+
+ 
+
+
